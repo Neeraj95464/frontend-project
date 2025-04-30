777a37f2f0a764a7bc6fc5ae1513204e13aac494
Neeraj95464 <neerajkumar95464@gmail.com>
2025-04-30 11:28:43 +0530

    public api added

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 // Base API URL
-const API_URL = "http://localhost:7355/api";
+const API_URL = "http://103.211.37.123:7355/api";
 
 // Create Axios instance
 const api = axios.create({
