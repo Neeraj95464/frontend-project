e2295d95c2730aee37b53126904f0d1a4195040c
Neeraj95464 <neerajkumar95464@gmail.com>
2025-05-13 15:02:47 +0530

    server code change

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -4,7 +4,7 @@ import { toast } from "react-toastify";
 // Base API URL
 // const API_URL = "http://103.211.37.123:7355/api";
 // const API_URL = "http://localhost:7355/api";
-const API_URL = "https://latex-pens-draft-rpg.trycloudflare.com/";
+const API_URL = "https://latex-pens-draft-rpg.trycloudflare.com/api";
 
 // Create Axios instance
 const api = axios.create({
