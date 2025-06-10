45899b4f487f8c1507f9dd14ce3acf58fd88fc2d
Neeraj95464 <neerajkumar95464@gmail.com>
2025-06-10 10:58:40 +0530

    server code change

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -37,8 +37,8 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 // Base API URL
-const API_URL = "http://localhost:7355/api";
-// const API_URL = "https://numerous-gem-accompanied-mac.trycloudflare.com/api";
+// const API_URL = "http://localhost:7355/api";
+const API_URL = "https://numerous-gem-accompanied-mac.trycloudflare.com/api";
 
 // Create Axios instance
 const api = axios.create({
