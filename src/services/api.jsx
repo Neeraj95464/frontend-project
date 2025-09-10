70a9e20a28fd513ce882fd788cc7b0979ff2e9b7
Neeraj95464 <132138791+Neeraj95464@users.noreply.github.com>
2025-09-10 11:33:52 +0530

    Update api.jsx
    server change

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -39,7 +39,7 @@ import { toast } from "react-toastify";
 // Base API URL
 // const API_URL = "http://localhost:7355/api";
 const API_URL =
-  "https://bedroom-aluminum-sd-rows.trycloudflare.com/api";
+  "https://certainly-updating-computational-lost.trycloudflare.com/api";
 
 // Create Axios instance
 const api = axios.create({
