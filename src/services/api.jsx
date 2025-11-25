c3001b41b8b000c4a19fb0480fe8c5d15675bcac
Neeraj95464 <neerajkumar95464@gmail.com>
2025-11-25 12:12:47 +0530

    server address change

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 // Base API URL
-const API_URL = "http://localhost:7355/api";
-// const API_URL = "https://mahavir-asset.duckdns.org:7355/api";
+// const API_URL = "http://localhost:7355/api";
+const API_URL = "https://mahavir-asset.duckdns.org:7355/api";
 
 // Create Axios instance
 const api = axios.create({
